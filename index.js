b6d7fecf395ed9ad26d5ca30d4626b792016e641
Hanna <hanna@WU2TEHPV02CX-08.Redmond.corp.microsoft.com>
2018-12-09 15:26:16 +0000

    wow

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import { readRap } from './src/gen'
 const say = require('say')
-
+const fs = require('fs')
+const player = require('player');
+ 
 const len = 200
+
+// create player instance
+const p = new player('./fresh.mp3');
+p.play()
 
 const readLine = (lines, i) => {
     if(i >= lines.length) {
+        p.stop()
         return
     }
     console.log(lines[i])
     say.speak(lines[i], undefined, 1.3, () => readLine(lines, i + 1))
 }
 
-readRap((contents) => {
+const parseRap = ((contents) => {
     let x = 0
     let prior = -1
     let lines = []
@@ -27,3 +34,5 @@ readRap((contents) => {
     }
     readLine(lines, 0)
 })
+
+readRap(parseRap)
