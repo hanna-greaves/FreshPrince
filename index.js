bc845beeda27b0aa45d21f35f111a20c49a43ba8
Hanna <hanna@WU2TEHPV02CX-08.Redmond.corp.microsoft.com>
2018-12-09 13:44:20 +0000

    whee

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import { readRap } from './src/gen'
+const say = require('say')
+
 const len = 200
+
+const readLine = (lines, i) => {
+    if(i >= lines.length) {
+        return
+    }
+    console.log(lines[i])
+    say.speak(lines[i], undefined, 1.3, () => readLine(lines, i + 1))
+}
+
 readRap((contents) => {
     let x = 0
     let prior = -1
+    let lines = []
     while(x < len) {
         let index = -1 
-        while(index === prior) {
+        do {
             index = Math.trunc(Math.random() * contents.length)
-        }
+        } while(index === prior)
+        prior = index
         const s = contents[index]
         x += s.length
-        console.log(s)
+        lines.push(s)
     }
+    readLine(lines, 0)
 })
